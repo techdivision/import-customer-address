f6cd88e6d502da75608776fec04c362a85c4b78a
Tim Wagner <t.wagner@techdivision.com>
2019-09-19 16:06:37 +0200

    Remove deprecated classes and methods

--- src/Services/CustomerAddressBunchProcessor.php
+++ src/Services/CustomerAddressBunchProcessor.php
@@ -533,22 +533,6 @@ class CustomerAddressBunchProcessor implements CustomerAddressBunchProcessorInte
     }
 
     /**
-     * Load's and return's the EAV attribute option value with the passed code, store ID and value.
-     *
-     * @param string  $attributeCode The code of the EAV attribute option to load
-     * @param integer $storeId       The store ID of the attribute option to load
-     * @param string  $value         The value of the attribute option to load
-     *
-     * @return array The EAV attribute option value
-     * @deprecated Since 4.0.0
-     * @see \TechDivision\Import\Services\EavAwareProcessorInterface::loadAttributeOptionValueByEntityTypeIdAndAttributeCodeAndStoreIdAndValue()
-     */
-    public function loadEavAttributeOptionValueByAttributeCodeAndStoreIdAndValue($attributeCode, $storeId, $value)
-    {
-        return $this->getEavAttributeOptionValueRepository()->findOneByAttributeCodeAndStoreIdAndValue($attributeCode, $storeId, $value);
-    }
-
-    /**
      * Load's and return's the EAV attribute option value with the passed entity type ID, code, store ID and value.
      *
      * @param string  $entityTypeId  The entity type ID of the EAV attribute to load the option value for
