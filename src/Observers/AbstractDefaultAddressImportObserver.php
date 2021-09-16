93d717265ff575b2e242780d7fe134d0a967f9e3
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2021-09-16 19:09:30 +0200

    Cleanup code and 3.8.x compatibility

--- src/Observers/AbstractDefaultAddressImportObserver.php
+++ src/Observers/AbstractDefaultAddressImportObserver.php
@@ -3,19 +3,13 @@
 /**
  * TechDivision\Import\Customer\Address\Observers\AbstractDefaultAddressImportObserver
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * PHP version 5
+ * PHP version 7
  *
  * @author    Vadim Justus <v.justus@techdivision.com>
  * @author    Harald Deiser <h.deiser@techdivision.com>
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-customer-address
  * @link      http://www.techdivision.com
  */
@@ -35,7 +29,7 @@ use TechDivision\Import\Customer\Observers\AbstractCustomerImportObserver;
  * @author    Harald Deiser <h.deiser@techdivision.com>
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-customer-address
  * @link      http://www.techdivision.com
  */
--- src/Observers/DefaultBillingAddressImportObserver.php
+++ src/Observers/DefaultBillingAddressImportObserver.php
@@ -3,19 +3,13 @@
 /**
  * TechDivision\Import\Customer\Address\Observers\DefaultBillingAddressImportObserver
  *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- *
- * PHP version 5
+ * PHP version 7
  *
  * @author    Vadim Justus <v.justus@techdivision.com>
  * @author    Harald Deiser <h.deiser@techdivision.com>
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-customer-address
  * @link      http://www.techdivision.com
  */
@@ -31,7 +25,7 @@ use TechDivision\Import\Customer\Address\Utils\ColumnKeys;
  * @author    Harald Deiser <h.deiser@techdivision.com>
  * @author    Tim Wagner <t.wagner@techdivision.com>
  * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @license   https://opensource.org/licenses/MIT
  * @link      https://github.com/techdivision/import-customer-address
  * @link      http://www.techdivision.com
  */
