77f358bf84be8c2069b95ee8e954a9f1a0e2c116
Martin Eisenführer <m.eisenfuehrer@techdivision.com>
2022-03-11 18:19:33 +0100

    Codereview

--- src/Observers/CustomerAddressExportObserver.php
+++ src/Observers/CustomerAddressExportObserver.php
@@ -85,8 +85,8 @@ class CustomerAddressExportObserver extends AbstractCustomerAddressImportObserve
 
         $artefacts[] = $this->newArtefact(
             array(
-                ColumnKeys::ENTITY_ID                => $this->getValue(ColumnKeys::ENTITY_ID) ?? null,
-                ColumnKeys::INCREMENT_ID             => $this->getValue(ColumnKeys::INCREMENT_ID)?? null,
+                ColumnKeys::ENTITY_ID                => $this->getValue(ColumnKeys::ENTITY_ID),
+                ColumnKeys::INCREMENT_ID             => $this->getValue(ColumnKeys::INCREMENT_ID),
                 ColumnKeys::EMAIL                    => $email,
                 ColumnKeys::WEBSITE                  => $this->getValue(ColumnKeys::WEBSITE),
                 ColumnKeys::CITY                     => $this->getValue(ColumnKeys::ADDRESS_CITY),
@@ -107,8 +107,8 @@ class CustomerAddressExportObserver extends AbstractCustomerAddressImportObserve
                 ColumnKeys::ADDRESS_DEFAULT_SHIPPING => $this->getValue(ColumnKeys::ADDRESS_DEFAULT_SHIPPING)
             ),
             array(
-                ColumnKeys::ENTITY_ID                => $this->getValue(ColumnKeys::ENTITY_ID) ?? null,
-                ColumnKeys::INCREMENT_ID             => $this->getValue(ColumnKeys::INCREMENT_ID)?? null,
+                ColumnKeys::ENTITY_ID                => ColumnKeys::ENTITY_ID,
+                ColumnKeys::INCREMENT_ID             => ColumnKeys::INCREMENT_ID,
                 ColumnKeys::WEBSITE                  => ColumnKeys::WEBSITE,
                 ColumnKeys::EMAIL                    => ColumnKeys::EMAIL,
                 ColumnKeys::CITY                     => ColumnKeys::ADDRESS_CITY,
